648c3dc7e4577ff1e63c8a9786ecf2cc59aab41f
Julien <juliendiot@ut-biomet.org>
2021-06-05 15:23:09 +0900

    add data loading and gwas functions

--- README.md
+++ README.md
@@ -8,3 +8,184 @@
 
 The goal of GWAS-Engine is to provide simple “input/output” style code
 for GWAS analysis in order to be used inside an API.
+
+# How to use GWAS-Engine
+
+## Dependencies
+
+``` r
+# required packages
+library(gaston) # for many functions
+#> Loading required package: Rcpp
+#> Loading required package: RcppParallel
+#> 
+#> Attaching package: 'RcppParallel'
+#> The following object is masked from 'package:Rcpp':
+#> 
+#>     LdFlags
+#> Gaston set number of threads to 4. Use setThreadOptions() to modify this.
+#> 
+#> Attaching package: 'gaston'
+#> The following object is masked from 'package:stats':
+#> 
+#>     sigma
+#> The following objects are masked from 'package:base':
+#> 
+#>     cbind, rbind
+library(rjson) # manage json format
+library(manhattanly) # manhattan plot using plotly
+#> See example usage at http://sahirbhatnagar.com/manhattanly/
+```
+
+## Load R scripts
+
+Load all `.R` files in folder `src`.
+
+``` r
+invisible(
+  sapply(FUN = source,
+         X = list.files("src", pattern = ".R$",full.names = T))
+)
+```
+
+## Load data
+
+Example data are stored in the `data` folder.
+
+``` r
+gDta <- readGenoData("data/markers/testMarkerData01.vcf.gz")
+#> 2021-06-05 14:55:17 - r-readGenoData(): Check file extention ... 
+#> 2021-06-05 14:55:17 - r-readGenoData(): Read geno file ... 
+#> ped stats and snps stats have been set. 
+#> 'p' has been set. 
+#> 'mu' and 'sigma' have been set.
+#> 2021-06-05 14:55:18 - r-readGenoData(): Read geno file DONE 
+#> 2021-06-05 14:55:18 - r-readGenoData(): DONE, return output.
+pDta <- readPhenoData("data/pheno/testPhenoData01.csv")
+#> 2021-06-05 14:55:18 - r-readPhenoData(): Read phenotypic file ... 
+#> 2021-06-05 14:55:18 - r-readPhenoData(): Read phenotypic file DONE 
+#> 2021-06-05 14:55:18 - r-readPhenoData(): DONE, return output.
+```
+
+For online data, they can be loaded using:
+
+``` r
+gDta <- readGenoData("data/markers/testMarkerData01.vcf.gz")
+pDta <- readPhenoData("data/pheno/testPhenoData01.csv")
+```
+
+## GWAS
+
+To run a GWAS analysis you should first process the data using
+`prepareData` and then call the `gwas` function.
+
+``` r
+data <- prepareData(gDta=gDta, pDta = pDta)
+#> 2021-06-05 14:55:18 - r-prepareData(): Remove from geno data individuals that are not in phenotypic data-set ...
+#> 2021-06-05 14:55:19 - r-prepareData(): Remove from geno data individuals that are not in phenotypic data-set DONE
+#> 2021-06-05 14:55:19 - r-prepareData(): reorder matrix ...
+#> 2021-06-05 14:55:19 - r-prepareData(): reorder matrix DONE
+#> 2021-06-05 14:55:19 - r-prepareData(): remove monomorphic geno ...
+#> 2021-06-05 14:55:19 - r-prepareData(): remove monomorphic geno DONE
+#> 2021-06-05 14:55:19 - r-prepareData(): calculate genetic relatinoal matrix ...
+#> 2021-06-05 14:55:20 - r-prepareData(): calculate genetic relatinoal matrix DONE
+#> 2021-06-05 14:55:20 - r-prepareData(): DONE, return output.
+
+gwasRes <- gwas(data = data,
+                trait = "Panicle.length",
+                test = "score",
+                fixed = 0,
+                response = "quantitative",
+                thresh_maf = 0.05,
+                thresh_callrate = 0.95)
+#> 2021-06-05 14:55:20 - r-gwas(): Check inputs ...
+#> 2021-06-05 14:55:20 - r-gwas(): Check inputs DONE
+#> 2021-06-05 14:55:20 - r-gwas(): aggregate data in bed matrix ...
+#> 2021-06-05 14:55:20 - r-gwas(): aggregate data DONE
+#> 2021-06-05 14:55:20 - r-gwas(): remove samples with missing phenotypic values ...
+#> 2021-06-05 14:55:20 - r-gwas(): remove samples with missing phenotypic values DONE
+#> 2021-06-05 14:55:20 - r-gwas(): filter SNPs ...
+#> 2021-06-05 14:55:20 - r-gwas(): filter SNPs DONE
+#> 2021-06-05 14:55:20 - r-gwas(): fit model ...
+#> [Iteration 1] theta = 6.11889 3.12119
+#> [Iteration 1] log L = -545.19
+#> [Iteration 1] AI-REML update
+#> [Iteration 1] ||gradient|| = 4.44998
+#> [Iteration 2] theta = 2.40545 5.61423
+#> [Iteration 2] log L = -536.314
+#> [Iteration 2] AI-REML update
+#> [Iteration 2] ||gradient|| = 5.68172
+#> [Iteration 3] theta = 2.61764  6.5878
+#> [Iteration 3] log L = -534.365
+#> [Iteration 3] AI-REML update
+#> [Iteration 3] ||gradient|| = 0.53883
+#> [Iteration 4] theta = 2.60704 6.83002
+#> [Iteration 4] log L = -534.325
+#> [Iteration 4] AI-REML update
+#> [Iteration 4] ||gradient|| = 0.0362724
+#> [Iteration 5] theta =  2.5918 6.86787
+#> [Iteration 5] log L = -534.324
+#> [Iteration 5] AI-REML update
+#> [Iteration 5] ||gradient|| = 0.0110436
+#> [Iteration 6] theta = 2.58775 6.87669
+#> [Iteration 6] log L = -534.324
+#> [Iteration 6] AI-REML update
+#> [Iteration 6] ||gradient|| = 0.00279298
+#> [Iteration 7] theta = 2.58674 6.87888
+#> [Iteration 7] log L = -534.324
+#> [Iteration 7] AI-REML update
+#> [Iteration 7] ||gradient|| = 0.000699933
+#> [Iteration 8] theta = 2.58648 6.87942
+#> [Iteration 8] log L = -534.324
+#> [Iteration 8] AI-REML update
+#> [Iteration 8] ||gradient|| = 0.000175022
+#> [Iteration 9] theta = 2.58642 6.87956
+#> [Iteration 9] log L = -534.324
+#> [Iteration 9] AI-REML update
+#> [Iteration 9] ||gradient|| = 4.37413e-05
+#> [Iteration 10] theta = 2.5864 6.8796
+#> [Iteration 10] log L = -534.324
+#> [Iteration 10] AI-REML update
+#> [Iteration 10] ||gradient|| = 1.09303e-05
+#> [Iteration 11] theta = 2.5864 6.8796
+#> [Iteration 11] log L = -534.324
+#> [Iteration 11] AI-REML update
+#> [Iteration 11] ||gradient|| = 2.73122e-06
+#> 2021-06-05 14:55:21 - r-gwas(): fit model DONE
+#> 2021-06-05 14:55:21 - r-gwas(): DONE, return output.
+head(gwasRes)
+#>   chr   pos           id A1 A2    freqA2      score         p
+#> 1   1  9563  SNP-1.8562.  A  T 0.1382682 1.45574550 0.2276083
+#> 2   1 25922 SNP-1.24921.  C  T 0.1278090 0.36309704 0.5467912
+#> 3   1 26254 SNP-1.25253.  A  T 0.2955182 0.02865046 0.8655886
+#> 4   1 30214 SNP-1.29213.  T  A 0.1344538 0.51074493 0.4748166
+#> 5   1 31478 SNP-1.30477.  C  T 0.2262570 0.21535471 0.6426024
+#> 6   1 32733 SNP-1.31732.  T  G 0.3011204 0.05018145 0.8227478
+```
+
+## Manhattan plot
+
+``` r
+#* Manhattan plot (type 2)
+#* @tag Plots
+#* @param modelS3Path url of the model data file (rds file)
+#* @param adj_method either bonferroni or FDR
+#* @param thresh_p
+#* @param chr names of the chromosomes to show separated using comma. Show all chromosomes if nothing is specified.
+#* @serializer htmlwidget
+#* @get /manplot
+# function(res, modelS3Path, adj_method, thresh_p = 0.05, chr = NA){
+```
+
+## LD PLot
+
+``` r
+#* LD plot
+#* @tag Plots
+#* @param geno_url url of the markers data file (.vcf.gz file)
+#* @param from (total number of SNP should be < 50)
+#* @param to (total number of SNP should be < 50)
+#* @serializer png
+#* @get /LDplot
+# function(res, geno_url, from, to){
+```
---
+++ src/fun_dataLoading.R
@@ -0,0 +1,301 @@
+# Author: Julien Diot juliendiot@ut-biomet.org
+# 2019 The University of Tokyo
+#
+# Description:
+# Functions of the GWAS API related to data loading
+
+
+#' Download geno data
+#'
+#' @param url url of the geno data file (.vcf.gz file)
+#'
+#' @return `gaston::bed.matrix`
+downloadGenoData <- function(url) {
+  logger <- logger$new("r-downloadGenoData()")
+  logger$log("Create local temp file ... ")
+  localFile <- tempfile(pattern = "geno",
+                        tmpdir = tempdir(),
+                        fileext = ".vcf.gz")
+  logger$log("Download geno file ...")
+
+  # download data file:
+  tryCatch({
+    download.file(url, localFile)
+  }, error = function(err) {
+    logger$log("Download geno file FAILED.\n\tURL is:", url,
+               "\nError message is:\n\t", err$message,
+               redis = TRUE,
+               status = "FAILED",
+               action_type = "GET_MARKER_DTA")
+    return(NULL)
+  })
+
+  dta <- readGenoData(localFile)
+
+  dta
+}
+
+#' Download phenotypic data
+#'
+#' @param url url of the phenotypic data file (csv file)
+#'
+#' @return `data.frame`
+downloadPhenoData <- function(url){
+  logger <- logger$new("r-getPhenoData()")
+  logger$log("Create local temp file ... ")
+  localFile <- tempfile(pattern = "pheno",
+                        tmpdir = tempdir(),
+                        fileext = ".csv")
+
+  # download data file:
+  logger$log("Download phenotypic file ... ")
+  tryCatch({
+    download.file(url, localFile)
+  }, error = function(err) {
+    logger$log("Download geno file FAILED.\n\tURL is:", url,
+               "\nError message is:\n\t", err$message,
+               redis = TRUE,
+               status = "FAILED",
+               action_type = "GET_PHENO_DTA")
+    return(NULL)
+  })
+
+  dta <- readPhenoData(localFile)
+
+  logger$log("DONE, return output.")
+  dta
+}
+
+
+
+#' Download data for GWAS model
+#'
+#' @param genoUrl
+#' @param phenoUrl
+#'
+#' @return List
+downloadData <- function(genoUrl, phenoUrl){
+  logger <- logger$new("r-loadData()")
+  logger$log("get geno data ...")
+  mDta <- downloadGenoData(genoUrl)
+  logger$log("get geno data DONE")
+
+  logger$log("get pheno data ...")
+  pDta <- downloadPhenoData(phenoUrl)
+  logger$log("get pheno data DONE")
+
+
+  # Remove from geno data individuals that are not in phenotypic data-set
+  logger$log("Remove from geno data individuals that are not in phenotypic data-set ...")
+  mDta <- select.inds(mDta, id %in% rownames(pDta))
+  logger$log("Remove from geno data individuals that are not in phenotypic data-set DONE")
+
+
+  # reorder phenotypic data with id in bed matrix
+  logger$log("reorder matrix ...")
+  pDta <- pDta[mDta@ped$id,]
+  logger$log("reorder matrix DONE")
+
+
+  # remove monomorphic geno
+  logger$log("remove monomorphic geno ...")
+  mDta <- select.snps(mDta, maf > 0)
+  logger$log("remove monomorphic geno DONE")
+
+
+  # calculate genetic relational matrix
+  logger$log("calculate genetic relatinoal matrix ...")
+  grm <- GRM(mDta)
+  logger$log("calculate genetic relatinoal matrix DONE")
+
+  logger$log("DONE, return output.")
+
+  list(genoData = mDta, phenoData = pDta, grMatrix = grm)
+}
+
+
+#' Download a gwas reults
+#'
+#' @param url url of the model data file (json file)
+#'
+#' @return `data.frame`
+downloadGWAS <- function(url){
+  logger <- logger$new("r-loadModel()")
+  logger$log("Create local temp file ... ")
+  localFile <- tempfile(pattern = "downloadedModel",
+                        tmpdir = tempdir(),
+                        fileext = ".json")
+  logger$log("Download model file ... ")
+  download.file(url, localFile)
+
+  logger$log("Read model file ... ")
+  gwasRes <- readLines(localFile)
+  logger$log("Read model file DONE ")
+  logger$log("Convert Json to data.frame ... ")
+  gwasRes <- fromJSON(gwasRes)
+  gwasRes$gwas <- as.data.frame(gwasRes$gwas)
+  logger$log("Convert Json to data.frame DONE ")
+
+  logger$log("DONE, return output.")
+  gwasRes
+}
+
+
+
+
+
+#' Read geno data from a file
+#'
+#' @param file VCF file path (ext `.vcf` or `.vcf.gz`)
+#'
+#' @return `gaston::bed.matrix`
+readGenoData <- function(file) {
+  logger <- logger$new("r-readGenoData()")
+
+  logger$log("Check file extention ... ")
+  ext <- tools::file_ext(file)
+  if (identical(ext, "gz")) {
+    ext <- paste0(
+      tools::file_ext(tools::file_path_sans_ext(file, compression = FALSE)),
+      ".", ext)
+  }
+
+  if (!(identical(ext, "vcf.gz") || identical(ext, "vcf"))) {
+    stop("Genotypic file should be in VCF format. `.vcf`, or `.vcf.gz`. Provided file extention is ", ext)
+  }
+
+  logger$log("Read geno file ... ")
+
+  dta <- gaston::read.vcf(file,
+                          verbose = TRUE,
+                          convert.chr = FALSE)
+  logger$log("Read geno file DONE ")
+  logger$log("DONE, return output.")
+
+  dta
+}
+
+
+#' Title
+#'
+#' @param file file path
+#' @param row.names [default 1] a single number giving the column of the table which contains the row names
+#' @param ... Further arguments to be passed to `read.csv`
+#'
+#' @return `data.frame`
+readPhenoData <- function(file, row.names = 1, ...) {
+  logger <- logger$new("r-readPhenoData()")
+  # read data file:
+  logger$log("Read phenotypic file ... ")
+  dta <- read.csv(file,
+                  row.names = row.names,
+                  ...)
+  logger$log("Read phenotypic file DONE ")
+  logger$log("DONE, return output.")
+  dta
+}
+
+
+#' Download data for GWAS model
+#'
+#' @param genoFile
+#' @param phenoFile
+#'
+#' @return List
+readData <- function(genoFile, phenoFile){
+  logger <- logger$new("r-readData()")
+  logger$log("get geno data ...")
+  mDta <- getGenoData(genoFile)
+  logger$log("get geno data DONE")
+
+  logger$log("get pheno data ...")
+  pDta <- readPhenoData(phenoFile)
+  logger$log("get pheno data DONE")
+
+
+  # Remove from geno data individuals that are not in phenotypic data-set
+  logger$log("Remove from geno data individuals that are not in phenotypic data-set ...")
+  mDta <- select.inds(mDta, id %in% rownames(pDta))
+  logger$log("Remove from geno data individuals that are not in phenotypic data-set DONE")
+
+
+  # reorder phenotypic data with id in bed matrix
+  logger$log("reorder matrix ...")
+  pDta <- pDta[mDta@ped$id,]
+  logger$log("reorder matrix DONE")
+
+
+  # remove monomorphic geno
+  logger$log("remove monomorphic geno ...")
+  mDta <- select.snps(mDta, maf > 0)
+  logger$log("remove monomorphic geno DONE")
+
+
+  # calculate genetic relational matrix
+  logger$log("calculate genetic relatinoal matrix ...")
+  grm <- GRM(mDta)
+  logger$log("calculate genetic relatinoal matrix DONE")
+
+  logger$log("DONE, return output.")
+
+  list(genoData = mDta, phenoData = pDta, grMatrix = grm)
+}
+
+
+#' Title
+#'
+#' @param file json file generated by the function `gwas` containing GWAS result
+#'
+#' @return `data.frame`
+readGWAS <- function(file) {
+  logger <- logger$new("r-readGWAS()")
+
+  logger$log("Read model file ... ")
+  gwasRes <- readLines(file)
+  logger$log("Read model file DONE ")
+  logger$log("Convert Json to data.frame ... ")
+  gwasRes <- fromJSON(gwasRes)
+  gwasRes$gwas <- as.data.frame(gwasRes$gwas)
+  logger$log("Convert Json to data.frame DONE ")
+
+  logger$log("DONE, return output.")
+  gwasRes
+}
+
+
+
+#' Filter individuals and calculate genetic relatinoal matrix
+#'
+#' @param gDta output of `downloadGenoData` or `readGenoData` functions
+#' @param pDta output of `downloadPhenoData` or `readPhenoData` functions
+#'
+#' @return List of 3 elements: `genoData`, `phenoData`, `grMatrix`
+prepareData <- function(gDta, pDta) {
+  logger <- logger$new("r-prepareData()")
+  # Remove from geno data individuals that are not in phenotypic data-set
+  logger$log("Remove from geno data individuals that are not in phenotypic data-set ...")
+  gDta <- select.inds(gDta, id %in% rownames(pDta))
+  logger$log("Remove from geno data individuals that are not in phenotypic data-set DONE")
+
+
+  # reorder phenotypic data with id in bed matrix
+  logger$log("reorder matrix ...")
+  pDta <- pDta[gDta@ped$id,]
+  logger$log("reorder matrix DONE")
+
+
+  # remove monomorphic geno
+  logger$log("remove monomorphic geno ...")
+  gDta <- select.snps(gDta, maf > 0)
+  logger$log("remove monomorphic geno DONE")
+
+
+  # calculate genetic relational matrix
+  logger$log("calculate genetic relatinoal matrix ...")
+  grm <- GRM(gDta)
+  logger$log("calculate genetic relatinoal matrix DONE")
+
+  logger$log("DONE, return output.")
+
+  list(genoData = gDta, phenoData = pDta, grMatrix = grm)
+}
