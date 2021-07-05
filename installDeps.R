a6492312250176225e182d378e7f0988bb9f6311
Julien <juliendiot@ut-biomet.org>
2021-07-05 13:48:31 +0900

    add install dependencies script

---
+++ installDeps.R
@@ -0,0 +1,7 @@
+# Author: Julien Diot juliendiot@ut-biomet.org
+# 2021 The University of Tokyo
+#
+# Description:
+# Install GWAS-engine dependencies
+
+install.packages(c("gaston", "digest", "jsonlite", "manhattanly", "testthat", "tools", "R6"))
--- tests/testthat/test_0_dependencies.R
+++ tests/testthat/test_0_dependencies.R
@@ -9,8 +9,10 @@
 capture_output({
   test_that("dependencies", {
     expect_true(compareVersion("1.5.7", as.character(packageVersion("gaston"))) != 1)
+    expect_true(compareVersion("0.6.27", as.character(packageVersion("digest"))) != 1)
     expect_true(compareVersion("1.7.2", as.character(packageVersion("jsonlite"))) != 1)
     expect_true(compareVersion("0.3.0", as.character(packageVersion("manhattanly"))) != 1)
-    expect_true(compareVersion("0.3.0", as.character(packageVersion("manhattanly"))) != 1)
+    expect_true(compareVersion("4.0.4", as.character(packageVersion("tools"))) != 1)
+    expect_true(compareVersion("2.5.0", as.character(packageVersion("R6"))) != 1)
   })
 })
