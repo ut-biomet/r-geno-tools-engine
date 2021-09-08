deb07e2b6d7e500673f585836a4ea255818778bc
Julien <juliendiot@ut-biomet.org>
2021-09-08 20:24:46 +0900

    update documentation

--- gwas-engine.R
+++ gwas-engine.R
@@ -27,15 +27,15 @@ p <- add_argument(p, "--chr", help="[`manplot`] name of the chromosome to show (
 p <- add_argument(p, "--test", help='[`gwas`] Which test to use. Either `"score"`, `"wald"` or `"lrt"`. For binary phenotypes, test = `"score"` is mandatory. For more information about this parameters see: https://www.rdocumentation.org/packages/gaston/versions/1.4.9/topics/association.test' , type = "character")
 p <- add_argument(p, "--fixed", help='[`gwas`] Number of Principal Components to include in the model with fixed effect (for test = `"wald"` or `"lrt"`). Default value is 0. For more information about this parameters see: https://www.rdocumentation.org/packages/gaston/versions/1.4.9/topics/association.test', type = "integer", default = 0)
 p <- add_argument(p, "--response", help='[`gwas`] Either "quantitative" or "binary". Is the trait a quantitative or a binary phenotype? Default value is "quantitative"' , type = "character", default = "quantitative")
-p <- add_argument(p, "--adj_method", help='[`manplot`, `adjresults`] correction method: "holm", "hochberg", "bonferroni", "BH", "BY", "fdr", "none". Default: "bonferroni". (see https://www.rdocumentation.org/packages/stats/versions/3.6.2/topics/p.adjust for more details)', type = "character", default = "bonferroni")
+p <- add_argument(p, "--adj_method", help='[`manplot`, `adjresults`] p-value correction method: "holm", "hochberg", "bonferroni", "BH", "BY", "fdr", "none". Default: "bonferroni". (see https://www.rdocumentation.org/packages/stats/versions/3.6.2/topics/p.adjust for more details)', type = "character", default = "bonferroni")
 
 p <- add_argument(p, "--thresh_maf", help="[`gwas`] Threshold for filtering markers. Only markers with minor allele frequency > `thresh_maf` will be kept.", type = "numeric")
 p <- add_argument(p, "--thresh_callrate", help="[`gwas`] Threshold for filtering markers. Only markers with a callrate > `thresh_callrate` will be kept.", type = "numeric")
 p <- add_argument(p, "--thresh_p", help="[`manplot`, `adjresults`] p value significant threshold (default 0.05)", type = "numeric", default = 0.05)
 
 
-p <- add_argument(p, "--from", help="[`ldplot`] lower bound of the range of SNPs for which the LD is computed", type = "integer")
-p <- add_argument(p, "--to", help="[`ldplot`] upper bound of the range of SNPs for which the LD is computed", type = "integer")
+p <- add_argument(p, "--from", help="[`ldplot`] lower bound of the range of SNPs for which the LD is computed (`from` must be lower than `to`)", type = "integer")
+p <- add_argument(p, "--to", help="[`ldplot`] upper bound of the range of SNPs for which the LD is computed (the total number of SNP should be lower than 50)", type = "integer")
 
 
 # Parse the command line arguments
